9e67629cbe036adb87371c081d2746b798741418
jienhua <jien8013@hotmail.com>
2016-11-28 11:57:34 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express    = require('express')
+const bodyParser = require('body-parser')
+const request    = require('request')
+const app        = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', (req, res) => {
+	res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', (req, res) => {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me'){
+		res.send(req.qeury['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), ()=>{
+	console.log('running on port', app.get('port'))
+})
